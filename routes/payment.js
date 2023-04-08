61a95e90466059d17890813368622d3654e62732
Laurine <laurinem.moulin@gmail.com>
2023-04-08 10:23:27 +0200

    adapting payment route with my own account

--- routes/payment.js
+++ routes/payment.js
@@ -7,16 +7,18 @@ router.use(express.json());
 
 //! A tester - je ne me souviens pas si elle est fonctionnelle
 
-router.post("/payment", isAuthenticated, async (req, res) => {
+router.post("/payment", async (req, res) => {
+  console.log(req.body);
   try {
     // Je reçois un token du front
     const stripeToken = req.body.stripeToken;
-    const priceToPayInCents = req.body.amount * 100;
+    // const stripeToken = req.body.token;
+    const priceToPayInCents = (req.body.amount * 100).toFixed(0);
     // Je fais une requête à stripe pour créer un paiement
-    const responseFromStripe = await stripe.charges.create({
+    let responseFromStripe = await stripe.charges.create({
       amount: priceToPayInCents,
       currency: "eur",
-      description: req.body.title,
+      description: `Paiement Vinted pour : ${req.body.title}`,
       source: stripeToken,
     });
     // Si le paiement est effectué, on met à jour l'offre et on renvoie au front le fait que tout s'est bien passé
